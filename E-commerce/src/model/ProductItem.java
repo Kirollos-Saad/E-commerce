9a45aedcce6d9e77539ca3716fb5db2494e037b7
Ahmed Gabr <es_Ahmed.Gabr2025@alexu.edu.eg>
2022-12-15 18:43:13 +0200

    Added Method for Reading Data to DataBase.java, and added ProductItem.java

---
+++ E-commerce/src/model/ProductItem.java
@@ -0,0 +1,23 @@
+
+package model;
+
+public class ProductItem {
+   private String name;
+   private int id;
+   private double price;
+
+    public ProductItem(int id, String name, double price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+    
+    @Override
+    public String toString(){
+        return id + "," + name + "," + price;
+    
+    }
+    
+   
+          
+}
--- E-commerce/src/viewmodel/DataBase.java
+++ E-commerce/src/viewmodel/DataBase.java
@@ -2,10 +2,13 @@ package viewmodel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.sql.PreparedStatement;
+import model.ProductItem;
 
 public class DataBase {
 
@@ -36,9 +39,8 @@ public class DataBase {
     }
 
     public boolean writeRow(int id, String name, double price) {
-        if (connection == null) {
-            connect();
-        }
+
+        connect();
         String InsertQuery = "INSERT INTO products(id, name, price) VALUES (?,?,?)";
         PreparedStatement prepStatement;
         try {
@@ -50,9 +52,42 @@ public class DataBase {
             prepStatement.close();
             return true;
         } catch (SQLException ex) {
+            System.out.println("Writing Error: Row not added");
             return false;
         }
 
+    }
+
+    public ProductItem[] readAllData() {
+        connect();
+        try {
+            //Counting Number of Rows in data
+            Statement countStatement = connection.createStatement();
+            ResultSet countResultSet = countStatement.executeQuery("select count(*) from products");
+            countResultSet.next();
+            int count = countResultSet.getInt(1);
+            countResultSet.close();
+            ProductItem[] productData = new ProductItem[count];
+            
+            
+            //Getting data
+            Statement readStatement = connection.createStatement();
+            ResultSet readResultSet = readStatement.executeQuery("SELECT * FROM products");
+            int i = 0;
+            while (readResultSet.next()) {
+                int productId = readResultSet.getInt("id");
+                String productName = readResultSet.getString("name");
+                double productPrice = readResultSet.getFloat("price");                
+                productData[i++] = new ProductItem(productId, productName, productPrice);                
+            }
+            readResultSet.close();
+            return productData;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DataBase.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
     }
 
 }
